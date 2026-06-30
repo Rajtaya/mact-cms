d3332da5145364d2394be75828173c4511f37c79
Rajtaya <Rajtaya@kuk.ac.in>
2026-06-30 15:46:22 +0530

    fix: default-import cookie-parser (esModuleInterop)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
